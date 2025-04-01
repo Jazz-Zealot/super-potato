8daa3d0f94523ba0ca9731795229181a1917e827
Jazz-Zealot <py.cai@outlook.com>
2025-04-01 09:15:52 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 export const sentences = [
   {
     id: 1,
-    sentence: "After six months of arguing and final 16 hours of hot parliamentary debates, Australia’s Northern Territory became the first legal authority in the world to allow doctors to take the lives of incurably ill patients who wish to die.",
+    sentence: "After six months of arguing and final 16 hours of hot parliamentary debates, Australia's Northern Territory became the first legal authority in the world to allow doctors to take the lives of incurably ill patients who wish to die.",
     parts: [
       "After six months of arguing,",
       "and final 16 hours of hot parliamentary debates,",
-      "Australia’s Northern Territory became the first legal authority in the world,",
+      "Australia's Northern Territory became the first legal authority in the world,",
       "to allow doctors to take the lives of incurably ill patients who wish to die."
     ],
     audio: "audio/D1.mp3",
@@ -25,278 +25,26 @@ export const sentences = [
   },
   {
     id: 3,
-    sentence: "For example, when an American uses the word ‘friend’, the cultural implications of the word may be quite different from those it has in the visitor’s language and culture.",
+    sentence: "For example, when an American uses the word 'friend', the cultural implications of the word may be quite different from those it has in the visitor's language and culture.",
     parts: [
       "For example,",
-      "when an American uses the word ‘friend’,",
+      "when an American uses the word 'friend',",
       "the cultural implications of the word may be quite different",
-      "from those it has in the visitor’s language and culture."
+      "from those it has in the visitor's language and culture."
     ],
     audio: "audio/D3.mp3",
     image: "images/sentence3.png"
   },
   {
     id: 4,
-    sentence: "The phrase “substance abuse” is often used instead of “drug abuse” to make clear that substances such as alcohol and tobacco can be just as harmfully misused as heroin and cocaine.",
+    sentence: "The phrase "substance abuse" is often used instead of "drug abuse" to make clear that substances such as alcohol and tobacco can be just as harmfully misused as heroin and cocaine.",
     parts: [
-      "The phrase “substance abuse” is often used instead of “drug abuse”",
+      "The phrase "substance abuse" is often used instead of "drug abuse"",
       "to make clear that substances such as alcohol and tobacco",
       "can be just as harmfully misused as heroin and cocaine."
     ],
     audio: "audio/D4.mp3",
     image: "images/sentence4.png"
   },
-  {
-    id: 5,
-    sentence: "We live in a society in which the medicinal and social use of substances (drugs) is pervasive: an aspirin to quiet a headache, some wine to be sociable, coffee to get going in the morning, a cigarette for the nerves.",
-    parts: [
-      "We live in a society",
-      "in which the medicinal and social use of substances (drugs) is pervasive:",
-      "an aspirin to quiet a headache,",
-      "some wine to be sociable,",
-      "coffee to get going in the morning,",
-      "a cigarette for the nerves."
-    ],
-    audio: "audio/D5.mp3",
-    image: "images/sentence5.png"
-  },
-  {
-    id: 6,
-    sentence: "Dependence is marked first by an increased tolerance, with more and more of the substance required to produce the desired effect, and then by the appearance of unpleasant withdrawal symptoms when the substance is discontinued.",
-    parts: [
-      "Dependence is marked first by an increased tolerance,",
-      "with more and more of the substance required to produce the desired effect,",
-      "and then by the appearance of unpleasant withdrawal symptoms",
-      "when the substance is discontinued."
-    ],
-    audio: "audio/D6.mp3",
-    image: "images/sentence6.png"
-  },
-  {
-    id: 7,
-    sentence: "Drugs (substances) that affect the central nervous system and alter perception, mood, and behavior are known as psychoactive substances.",
-    parts: [
-      "Drugs (substances) that affect the central nervous system",
-      "and alter perception, mood, and behavior",
-      "are known as psychoactive substances."
-    ],
-    audio: "audio/D7.mp3",
-    image: "images/sentence7.png"
-  },
-  {
-    id: 8,
-    sentence: "The test of any democratic society lies not in how well it can control expression but in whether it gives freedom of thought and expression the widest possible latitude, however disputable or irritating the results may sometimes be.",
-    parts: [
-      "The test of any democratic society lies not in how well it can control expression",
-      "but in whether it gives freedom of thought and expression the widest possible latitude,",
-      "however disputable or irritating the results may sometimes be."
-    ],
-    audio: "audio/D8.mp3",
-    image: "images/sentence8.png"
-  },
-  {
-    id: 9,
-    sentence: "Though some of us have known for many, many years that the freedoms under the First Amendment are not totally unlimited, I think it is perhaps the case that some people associated with the company have only recently come to realize this.",
-    parts: [
-      "Though some of us have known for many, many years",
-      "that the freedoms under the First Amendment are not totally unlimited,",
-      "I think it is perhaps the case",
-      "that some people associated with the company have only recently come to realize this."
-    ],
-    audio: "audio/D9.mp3",
-    image: "images/sentence9.png"
-  },
-  {
-    id: 10,
-    sentence: "Much of the language used to describe monetary policy, such as “steering the economy to a soft landing” or “a touch on the brakes”, makes it sound like a precise science.",
-    parts: [
-      "Much of the language used to describe monetary policy,",
-      "such as “steering the economy to a soft landing” or “a touch on the brakes”,",
-      "makes it sound like a precise science."
-    ],
-    audio: "audio/D10.mp3",
-    image: "images/sentence10.png"
-  },
-  {
-  id: 11,
-  sentence: "Perhaps it is humankind’s long suffering at the mercy of flood and drought that makes the idea of forcing the waters to do our bidding so fascinating.",
-  parts: [
-    "Perhaps it is humankind’s long suffering",
-    "at the mercy of flood and drought",
-    "that makes the idea of forcing the waters to do our bidding so fascinating."
-  ],
-  audio: "audio/D11.mp3",
-  image: "images/sentence11.png"
-},
-{
-  id: 12,
-  sentence: "The Aswan Dam, for example, stopped the Nile flooding but deprived Egypt of the fertile silt that floods left—all in return for a giant reservoir of disease which is now so full of silt that it barely generates electricity.",
-  parts: [
-    "The Aswan Dam, for example, stopped the Nile flooding",
-    "but deprived Egypt of the fertile silt that floods left—",
-    "all in return for a giant reservoir of disease",
-    "which is now so full of silt that it barely generates electricity."
-  ],
-  audio: "audio/D12.mp3",
-  image: "images/sentence12.png"
-},
-{
-  id: 13,
-  sentence: "The trouble is that part of the recent acceleration is due to the usual rebound that occurs at this point in a business cycle, and so is not conclusive evidence of a revival in the underlying trend.",
-  parts: [
-    "The trouble is that part of the recent acceleration",
-    "is due to the usual rebound that occurs at this point in a business cycle,",
-    "and so is not conclusive evidence",
-    "of a revival in the underlying trend."
-  ],
-  audio: "audio/D13.mp3",
-  image: "images/sentence13.png"
-},
-{
-  id: 14,
-  sentence: "New ways of organizing the workplace—all that re-engineering and downsizing —are only one contribution to the overall productivity of an economy, which is driven by many other factors such as joint investment in equipment and machinery, new technology, and investment in education and training.",
-  parts: [
-    "New ways of organizing the workplace—all that re-engineering and downsizing—",
-    "are only one contribution to the overall productivity of an economy,",
-    "which is driven by many other factors",
-    "such as joint investment in equipment and machinery,",
-    "new technology,",
-    "and investment in education and training."
-  ],
-  audio: "audio/D14.mp3",
-  image: "images/sentence14.png"
-},
-{
-  id: 15,
-  sentence: "A survey of news stories in 1996 reveals that the anti-science tag has been attached to many other groups as well, from authorities who advocated the elimination of the last remaining stocks of smallpox virus to Republicans who advocated decreased funding for basic research.",
-  parts: [
-    "A survey of news stories in 1996 reveals",
-    "that the anti-science tag has been attached to many other groups as well,",
-    "from authorities who advocated the elimination of the last remaining stocks of smallpox virus",
-    "to Republicans who advocated decreased funding for basic research."
-  ],
-  audio: "audio/D15.mp3",
-  image: "images/sentence15.png"
-},
-{
-  id: 16,
-  sentence: "The true enemies of science, argues Paul Ehrlich of Stanford University, a pioneer of environmental studies, are those who question the evidence supporting global warming, the depletion of the ozone layer and other consequences of industrial growth.",
-  parts: [
-    "The true enemies of science, argues Paul Ehrlich of Stanford University,",
-    "a pioneer of environmental studies,",
-    "are those who question the evidence supporting global warming,",
-    "the depletion of the ozone layer",
-    "and other consequences of industrial growth."
-  ],
-  audio: "audio/D16.mp3",
-  image: "images/sentence16.png"
-},
-{
-  id: 17,
-  sentence: "Scattered around the globe are more than 100 small regions of isolated volcanic activity known to geologists as hot spots.",
-  parts: [
-    "Scattered around the globe",
-    "are more than 100 small regions of isolated volcanic activity",
-    "known to geologists as hot spots."
-  ],
-  audio: "audio/D17.mp3",
-  image: "images/sentence17.png"
-},
-{
-  id: 18,
-  sentence: "The complementary coastlines and certain geological features that seem to span the ocean are reminders of where the two continents were once joined.",
-  parts: [
-    "The complementary coastlines",
-    "and certain geological features that seem to span the ocean",
-    "are reminders of where the two continents were once joined."
-  ],
-  audio: "audio/D18.mp3",
-  image: "images/sentence18.png"
-},
-{
-  id: 19,
-  sentence: "It is not possible to determine whether both continents are moving in opposite directions or whether one continent is stationary and the other is drifting away from it.",
-  parts: [
-    "It is not possible to determine",
-    "whether both continents are moving in opposite directions",
-    "or whether one continent is stationary",
-    "and the other is drifting away from it."
-  ],
-  audio: "audio/D19.mp3",
-  image: "images/sentence19.png"
-},
-{
-  id: 20,
-  sentence: "Specialists in history and economics, have shown two things: that the period from 1650 to 1750 was marked by great poverty, and that industrialization certainly did not worsen and may have actually improved the conditions for the majority of the populace.",
-  parts: [
-    "Specialists in history and economics have shown two things:",
-    "that the period from 1650 to 1750 was marked by great poverty,",
-    "and that industrialization certainly did not worsen",
-    "and may have actually improved the conditions for the majority of the populace."
-  ],
-  audio: "audio/D20.mp3",
-  image: "images/sentence20.png"
-},
-{
-  id: 21,
-  sentence: "At the same time, the American Law Institute—a group of judges, lawyers, and academics whose recommendations carry substantial weight—issued new guidelines for tort law stating that companies need not warn customers of obvious dangers or bombard them with a lengthy list of possible ones.",
-  parts: [
-    "At the same time, the American Law Institute—",
-    "a group of judges, lawyers, and academics whose recommendations carry substantial weight—",
-    "issued new guidelines for tort law",
-    "stating that companies need not warn customers of obvious dangers",
-    "or bombard them with a lengthy list of possible ones."
-  ],
-  audio: "audio/D21.mp3",
-  image: "images/sentence21.png"
-},
-{
-  id: 22,
-  sentence: "Some companies are limiting the risk by conducting online transactions only with established business partners who are given access to the company’s private intranet.",
-  parts: [
-    "Some companies are limiting the risk",
-    "by conducting online transactions only with established business partners",
-    "who are given access to the company’s private intranet."
-  ],
-  audio: "audio/D22.mp3",
-  image: "images/sentence22.png"
-},
-{
-  id: 23,
-  sentence: "Online culture thinks highly of the notion that the information flowing onto the screen comes there by specific request. Once commercial promotion begins to fill the screen uninvited, the distinction between the Web and television fades.",
-  parts: [
-    "Online culture thinks highly of the notion",
-    "that the information flowing onto the screen comes there by specific request.",
-    "Once commercial promotion begins to fill the screen uninvited,",
-    "the distinction between the Web and television fades."
-  ],
-  audio: "audio/D23.mp3",
-  image: "images/sentence23.png"
-},
-{
-  id: 24,
-  sentence: "The examples of Virtual Vineyards, Amazon.com, and other pioneers show that a Web site selling the right kind of products with the right mix of interactivity, hospitality, and security will attract online customers.",
-  parts: [
-    "The examples of Virtual Vineyards, Amazon.com, and other pioneers show",
-    "that a Web site selling the right kind of products",
-    "with the right mix of interactivity, hospitality, and security",
-    "will attract online customers."
-  ],
-  audio: "audio/D24.mp3",
-  image: "images/sentence24.png"
-},
-{
-  id: 25,
-  sentence: "An invisible border divides those arguing for computers in the classroom on the behalf of students’ career prospects and those arguing for computers in the classroom for broader reasons of radical educational reform.",
-  parts: [
-    "An invisible border divides those arguing for computers in the classroom",
-    "on the behalf of students’ career prospects",
-    "and those arguing for computers in the classroom",
-    "for broader reasons of radical educational reform."
-  ],
-  audio: "audio/D25.mp3",
-  image: "images/sentence25.png"
-}
-];
-
+...
 export default sentences;
